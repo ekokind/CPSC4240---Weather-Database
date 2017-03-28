f0db0a663baf0c9620e58267c445fbc98b2f0261
Ella <ekokind@g.clemson.edu>
2017-03-28 13:09:46 -0400

    Initial
    config is database attributes
    mysqlClass.inc.php has functions to be used

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+	$dbhost = "mysql1.cs.clemson.edu";
+	$dbuser = "cpsc424_eecr";
+	$dbpass = "cpsc420";
+	$database = "cpsc424_6jt3";
+
+?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<head>
+	<meta charset="UTF-8">
+	<title> Weather Database </title>
+</head>
+
+<?php
+	session_start();
+?>
+
+<body>
+	<?php
+		$query = "SELECT * FROM weather_attrs WHERE MAX(timestamp)";
+		$result = mysql_query($query);
+		$result_row = mysql_fetch_row($result) 
+
+		$timestamp = $result_row[0];
+		$temp = $result_row[1];
+		$gps = $result_row[2];
+		$pressure = $result_row[3];
+		$humidity = $result_row[4];
+	?>
+
+</body>
