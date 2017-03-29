68561fbba1b7bae750881cebd9303aabb29df5da
Ella <ekokind@g.clemson.edu>
2017-03-29 12:40:40 -0400

    index can pull from the database

--- index.php
+++ index.php
@@ -4,28 +4,46 @@
 	<title> Weather Database </title>
 </head>
 
-<?php
-	session_start();
-	include_once "mysqlClass.inc.php";
-?>
 
-<body>
+<html>
+	<body>
 	<?php
-		$query = "SELECT * FROM weather_attrs WHERE MAX(timestamp)";
-		$result = mysql_query($query);
-		$result_row = mysql_fetch_row($result) 
+	//	session_start(); //cant find this anywhere
+	//	include_once "mysqlClass.inc.php"; //has the functions
+		$link = mysqli_connect('localhost', 'root', '', 'cpsc424_6jt3');
+		if (mysqli_connect_errno()){
+			printf("Connect failed: %s\n", mysqli_connect_error());
+	    	exit();
+		}
 
-		$timestamp = $result_row[0];
-		$temp = $result_row[1];
-		$gps = $result_row[2];
-		$pressure = $result_row[3];
-		$humidity = $result_row[4];
+		echo 'Connected successfully';
 
+		if ($result = mysqli_query($link, "SELECT DATABASE()")) {
+	    	$row = mysqli_fetch_row($result);
+	    	printf("Default database is %s.\n", $row[0]);
+	    	mysqli_free_result($result);
+		}
+
+		mysqli_select_db($link, "cpsc424_6jt3");
+
+		$result = mysqli_query($link, "SELECT * FROM weather_attrs ORDER BY timestamp DESC LIMIT 1");
+		$row = mysqli_fetch_row($result);
+		$timestamp = $row[0];
 		echo $timestamp;
-		echo $temp;
-		echo $gps;
-		echo $pressure;
-		echo $humidity;
+		$temp = $row[1];
+		$gps = $row[2];
+		$pressure = $row[3];
+		$humidity = $row[4];
 	?>
 
-</body>
+	<ul>
+		<li>Timestamp: <?php echo $timestamp?></li>
+		<li>Temperature: <?php echo $temp?> &deg;F</li>
+		<li>GPS Location: <?php echo $gps?></li>
+		<li>Pressure: <?php echo $pressure?> in</li>
+		<li>Humidity: <?php echo $humidity?>%</li>
+	</ul>
+		
+	</body>
+	
+</html>
